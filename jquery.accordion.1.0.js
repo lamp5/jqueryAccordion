5c1a12a022546cf9c0fdb9854933842330b3a4f6
Szczepan Musial <szczepantoja@vp.pl>
2016-01-29 14:32:49 +0100

    initial commit files

---
+++ jquery.accordion.1.0.js
@@ -0,0 +1,55 @@
+/**
+ * @author Szczepan
+ */
+
+
+(function($){
+	
+	$.fn.jqueryAccordion = function(options){
+		options = $.extend({
+			autoOpen: true,
+			openChild: 1,
+			animation: 'fast'
+			
+		}, options);
+		
+		return this.find('> div').each(function(index){
+			var $elem = index;
+			var $child = $(this);
+			var $header = $child.find('> div:first');
+			var $body = $child.find('> div:last');
+			
+			var openAccordion = function(){
+				
+				if($child.hasClass('accordion-open')){
+					$child.removeClass('accordion-open');
+					$body.slideUp(options.animation);	
+				}
+				else{
+					$child.addClass('accordion-open');
+					$body.slideDown(options.animation);
+					
+				}
+				
+			};
+			
+			var initialOpenAccordion = function(){
+				if($elem == options.openChild-1){
+					$header.trigger('click');
+				}
+				
+			};
+			
+			
+			$header.bind('click', openAccordion);
+			
+			if(options.autoOpen){
+				initialOpenAccordion();
+			}
+			
+		});
+	
+	
+	}
+	
+})(jQuery);
